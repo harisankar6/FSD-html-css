7bea36113915f8693cf2f020da89ce2db49ebffc
harisankar6 <harisankarvijay6@gmail.com>
2026-07-20 15:02:13 +0530

    Introduction to JavaScript

---
+++ script.js
@@ -0,0 +1,87 @@
+// console.log("Hari");
+// console.log(2+3);
+
+// variables
+
+// var
+// let
+// const
+
+// var data = 10
+// console.log(data);
+// let data1 = "Hari"
+// console.log(data1);
+// const data2 = true
+// console.log(data2);
+
+// Data types 
+// let a = 1
+// console.log(a, typeof a);
+// Number
+
+// let b = "Entri"
+// console.log(b, typeof b);
+// String 
+
+// let c = true
+// console.log(c, typeof c);
+//Boolean
+
+// let d = {
+//     name:"Hari",
+//     class:"E86",
+//     mobile:987968775,
+//     isstudent:true
+// }
+// console.log(d, typeof d);
+// Object 
+
+// let e = [1,2,3,true, "Hari"]
+// console.log(e, typeof e);
+// Array but in js it is Object
+
+// let name
+// console.log(name);
+// undefined -- Only initialisation
+
+// let value = null
+// console.log(value, typeof value);
+// Null but in JS it is object(Because it is an error);
+
+// Datatypes of JavaScript
+// Number 
+// String 
+// Boolean 
+// Object 
+// Array - Object 
+// Undefined 
+// Null - Object 
+
+// Natufre of JS 
+// Synchronous - Blocking ie, line by line execution
+// Single threaded - One task at a time
+// Loosely typed
+
+// declaration  
+// var name;
+// console.log(name);
+// name = 10
+// initialisation
+// console.log(name);
+
+// var name = "Hari"
+// console.log(name);
+// var name = 10
+// console.log(name);
+// var - redeclaration and reassigning
+
+// let a = 10
+// console.log(a);
+// a = "Hari"
+// console.log(a);
+// let - reassigning, redeclaration not possible
+
+const a = 10
+console.log(a);
+a = 20
+console.log(a);
